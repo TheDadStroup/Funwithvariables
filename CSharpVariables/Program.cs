da2e9642c90701015b81c74080923afba2b51775
The Dad <Shihen1611@gmail.com>
2021-02-19 19:22:08 -0600

    initial upload

---
+++ CSharpVariables/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CSharpVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogName = "Powder";
+
+            int dogAge = 15;
+
+            char nickName = 'D';
+
+            bool lovesHiking = true;
+
+            double tennisBalls = 16;
+
+            decimal weight = 50.3m;
+
+            Console.WriteLine($"My dog's name is {dogName}, He is {dogAge} years old and has {tennisBalls} tennis balls and weighs {weight} and it is {lovesHiking} that he loves to hike");
+
+            Console.WriteLine($"");
+        }
+    }
+}
